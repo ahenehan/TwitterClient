d4524f0c9d36c4968e622ea6aab9b24916e59974
ahenehan <ahenehan@zynga.com>
2014-02-11 21:57:51 -0800

    Forgot these files

--- src/com/codepath/apps/twitterclient/fragments/HomeTimelineFragment.java
+++ src/com/codepath/apps/twitterclient/fragments/HomeTimelineFragment.java
@@ -9,11 +9,19 @@ import com.codepath.apps.twitterclient.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class HomeTimelineFragment extends TweetsListFragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inf, ViewGroup parent, Bundle savedInstanceState) {
 		TwitterClientApp.getRestClient().getHomeTimeline(new JsonHttpResponseHandler() {
 			@Override
 			public void onSuccess(JSONArray jsonTweets) {
@@ -21,8 +29,9 @@ public class HomeTimelineFragment extends TweetsListFragment {
 			}
 			
 		});
+		return super.onCreateView(inf, parent, savedInstanceState);
 	}
-
+	
 	@Override
 	void customLoadDataFromApi(long lastId) {
 		TwitterClientApp.getRestClient().getHomeTimeline(new JsonHttpResponseHandler() {
--- src/com/codepath/apps/twitterclient/fragments/MentionsFragment.java
+++ src/com/codepath/apps/twitterclient/fragments/MentionsFragment.java
@@ -9,11 +9,19 @@ import com.codepath.apps.twitterclient.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MentionsFragment extends TweetsListFragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inf, ViewGroup parent, Bundle savedInstanceState) {
 		TwitterClientApp.getRestClient().getMentions(new JsonHttpResponseHandler() {
 			@Override
 			public void onSuccess(JSONArray jsonTweets) {
@@ -21,8 +29,9 @@ public class MentionsFragment extends TweetsListFragment {
 			}
 			
 		});
+		return super.onCreateView(inf, parent, savedInstanceState);
 	}
-
+	
 	@Override
 	void customLoadDataFromApi(long lastId) {
 		TwitterClientApp.getRestClient().getMentions(new JsonHttpResponseHandler() {
